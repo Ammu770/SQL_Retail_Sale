896040efd3f010d0a0ef00925f1ca45dadbe847f
Ammu770 <patialshubham32@gmail.com>
2024-09-12 05:23:55 +0530

    project_1.sql

--- project_1.sql
+++ project_1.sql
@@ -1,35 +1,43 @@
+--Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05.
+
 SELECT * FROM retail_sales
 WHERE sale_date = '2022-11-05'
 
-
+--Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
 
 SELECT *
 FROM retail_sales
 WHERE category = 'Clothing'
 AND TO_CHAR(sale_date,'yyy-MM') ='2022-11'
-AND quantity>=10
+AND quantity>=4
 
-
+-- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
 SELECT category, 
 SUM(total_sale) AS net_sale,
 COUNT (*) as total_orders
 FROM retail_sales
 GROUP BY 1
 
+-- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
 
 SELECT ROUND(Avg (age),2)
 FROM retail_sales
 WHERE category = 'Beauty'
 
+-- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
 
+SELECT * FROM retail_sales
+WHERE total_sale > 1000
 
-
+-- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
 
 SELECT category,gender, COUNT(*) as total_trans
 FROM retail_sales
 GROUP BY category , gender
 ORDER BY 1
 
+	
+-- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year.
 
 SELECT * FROM
 (
@@ -43,6 +51,7 @@ SELECT
 	   ) as t1
 	   WHERE rank = 1
 
+-- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
 
 
 SELECT customer_id,
@@ -53,6 +62,7 @@ ORDER BY 2 DESC
 LIMIT 5
 
 
+-- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
 SELECT 
 category,
 COUNT(DISTINCT customer_id) as cnt_unique
@@ -60,7 +70,7 @@ FROM retail_sales
 GROUP BY 1
 
 
-
+-- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17).
 WITH hourly_sale
 AS
 (
